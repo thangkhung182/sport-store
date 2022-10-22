58c533daa8e77b6c5828f7f70ed69a713ab40a67
trungnq <honganh.mai@vhc.com.vn>
2022-10-22 23:18:59 +0700

    custom directive

--- src/store/store.component.ts
+++ src/store/store.component.ts
@@ -39,9 +39,7 @@ export class StoreComponent {
         this.changePage(1)
     }
 
-    get pageNumbers(): number[] {
-        return Array(Math.ceil(this.repository.getProducts(
-                this.selectedCategory).length / this.productsPerPage)
-            ).fill(0).map((x, i) => i + 1)
+    get pageCount(): number {
+        return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)     
     }
 }
--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { StoreComponent } from './store.component';
+import { CounterDirective } from './../util/counter.directive';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective],
     exports: [StoreComponent]
 })
 
