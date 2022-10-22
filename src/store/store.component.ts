b839242227f052a62b3926785c34508154f6d140
trungnq <honganh.mai@vhc.com.vn>
2022-10-22 23:06:22 +0700

    filter by category

--- src/store/store.component.ts
+++ src/store/store.component.ts
@@ -8,15 +8,40 @@ import { ProductRepository } from "src/app/model/product.repository";
 })
 
 export class StoreComponent {
+    selectedCategory: string | undefined
+    productsPerPage = 4
+    selectedPage = 1
+    
     constructor(private repository: ProductRepository) {
 
     }
 
     get products(): Product[] {
-        return this.repository.getProducts()
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage)
     }
 
     get categories(): string[] {
         return this.repository.getCategories()
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage
+    }
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = newSize
+        this.changePage(1)
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository.getProducts(
+                this.selectedCategory).length / this.productsPerPage)
+            ).fill(0).map((x, i) => i + 1)
     }
 }
